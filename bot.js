007bf870a9ef9a40ee41d8f0e8ea7db8bd46f29e
nooraburahmamohammad789 <46997922+nooraburahmamohammad789@users.noreply.github.com>
2019-01-26 16:19:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("538089842391908352")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`====================`);
 }, 30)
 })
 
